3b3c1ac2ff5e1eed5cf6ee88963bee82da0e3656
HarryForSWT <zym123hp@gmail.com>
2020-10-29 23:47:01 +0100

    Add Funktion und Post gelernt

--- src/main/java/com/example/demo/TopicService.java
+++ src/main/java/com/example/demo/TopicService.java
@@ -2,16 +2,24 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-       List<Topic> topics= Arrays.asList(new Topic("15258759672","Harry Potter","De"),
+       List<Topic> topics= new ArrayList<>(Arrays.asList(new Topic("15258759672","Harry Potter","De"),
                                 new Topic("15247140593","赵祎明","cn"),
-                                 new Topic("15247135869","张慧敏","Inner Mongolia"));
+                                 new Topic("15247135869","张慧敏","Inner Mongolia")));
 
     public List<Topic> getAllTopics(){
         return topics;
+    }
+
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id) ).findFirst().get();
+    }
+    public void addTopic(Topic topic){
+        topics.add(topic);
     }
 }
