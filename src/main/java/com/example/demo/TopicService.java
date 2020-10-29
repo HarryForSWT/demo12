e03cc87f6656b2ae75e33a546a42aa395915dcbd
HarryForSWT <zym123hp@gmail.com>
2020-10-29 18:51:12 +0100

    Verlegung von der Liste in den TopicService.

--- src/main/java/com/example/demo/TopicService.java
+++ src/main/java/com/example/demo/TopicService.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
+       List<Topic> topics= Arrays.asList(new Topic("15258759672","Harry Potter","De"),
+                                new Topic("15247140593","赵祎明","cn"),
+                                 new Topic("15247135869","张慧敏","Inner Mongolia"));
 
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
 }
